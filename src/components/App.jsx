45e094986bab59c6bcca59a57f86e45351e6cda6
Hurduz Oksana <hurduzoksana@gmail.com>
2022-12-27 20:04:24 +0200

    Update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,66 @@
-import Feedback from "./Feedback";
+import React, { Component } from 'react';
+import Statistics from './Statistics/Statistics';
+import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
+import Section from './Section/Section';
 
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      <Feedback/>
-    </div>
-  );
-};
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleIncrement = event => {
+    this.setState(prevState => ({
+      [event.target.name]: prevState[event.target.name] + 1,
+    }));
+  };
+
+  countTotalFeedback() {
+    return this.state.good + this.state.neutral + this.state.bad;
+  }
+
+  countPositiveFeedbackPercentage() {
+    const feedback = Math.floor(
+      (this.state.good / this.countTotalFeedback()) * 100
+    );
+    return feedback ? feedback : 0;
+  }
+
+  render() {
+    return (
+      <div
+        style={{
+          padding: 50,
+
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 20,
+        }}
+      >
+        <>
+          <Section title="Please leave feedback">
+            <FeedbackOptions
+              options={Object.keys(this.state)}
+              onLeaveFeedback={this.handleIncrement}
+            />
+          </Section>
+
+          <Section title="Statistics">
+            <Statistics
+              good={this.state.good}
+              neutral={this.state.neutral}
+              bad={this.state.bad}
+              totalfeedback={this.countTotalFeedback()}
+              feedbackPercentage={this.countPositiveFeedbackPercentage()}
+            />
+          </Section>
+        </>
+      </div>
+    );
+  }
+}
+
+export { App };
